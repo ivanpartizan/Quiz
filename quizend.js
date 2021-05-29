fdaf32b741fe4cfcd4aed277e39bb927a906a921
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-05-29 04:53:36 +0200

    update

---
+++ quizend.js
@@ -0,0 +1,10 @@
+const username = document.querySelector("#username");
+const saveScoreBtn = document.querySelector("#saveScoreBtn");
+const finalScore = document.querySelector("#finalScore");
+const mostRecentScore = document.querySelector("#mostRecentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
